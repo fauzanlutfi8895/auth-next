68b212f6807ef259e4324fcd9b5e5b5e1fae533d
fauzanlutfi8895 <fauzanlutfi8895@gmail.com>
2025-08-01 14:16:14 +0800

    middleware & credentials

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -34,7 +34,7 @@ export const LoginForm = () => {
 
     startTransition(() => {
       login(values).then(data => {
-        setError(data.error);
+        setError(data?.error);
         setSuccess(data.success);
       });
     });
