7781fe3acbbd0285195f3e65df825a9a3b3cc1ab
fauzanlutfi8895 <fauzanlutfi8895@gmail.com>
2025-08-04 14:01:45 +0800

    form reset password

--- routes.ts
+++ routes.ts
@@ -3,14 +3,14 @@
  * These routes dont required the authentication
  * @type {string[]}
  */
-export const publicRoutes = ["/", "/auth/new-verification", "/auth/new-password"];
+export const publicRoutes = ["/", "/auth/new-verification"];
 
 /**
  * An array of routes that are used for authentication
  * These routes will redirect logged in users to /settings
  * @type {string[]}
  */
-export const authRoutes = ["/auth/login", "/auth/register", "/auth/error", "/auth/reset"];
+export const authRoutes = ["/auth/login", "/auth/register", "/auth/error", "/auth/reset", "/auth/new-password"];
 
 /**
  * The prefix for API authentication routes
